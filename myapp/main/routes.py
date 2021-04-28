c77dbb470a46b4c3bd54fb22c4e6666a3e2ba4cc
Faiz <faizahmed11234@gmail.com>
2021-04-28 13:52:32 +0500

    Added jobs exporting to csv functionality

--- myapp/main/routes.py
+++ myapp/main/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, send_from_directory, current_app
 from myapp.models import Jobs
 from flask_login import login_required
-
+import os
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
@@ -20,18 +21,79 @@ def job_details(id):
     job = Jobs.query.get(id)
     return render_template ("main/job_details.html", job=job)
 
-@main.route('/results')
+@main.route('/results', methods=['GET', 'POST'])
 @login_required
 def results():
-    query = request.args.get('query').split()
-    location = request.args.get('location')
-    results = []
-    for q in query:
-        search = f"%{q}%"
-        que = Jobs.query.filter(Jobs.job_title.like(search)).filter \
-                        (Jobs.job_location.like(f"%{location}%")).all()
-        for i in que:
-            results.append(i)
-    keywords = ' '.join(query)
-    return render_template("main/results.html", jobs=results,
-                            length=len(results), keywords=keywords)
+    if request.method == 'POST':
+        jobs = request.form.getlist('csv_jobs')
+        mypath = 'myapp\\static\\myjobs.csv'
+        if os.path.exists(mypath): os.remove(mypath)
+        myjobs = list(map(lambda _id: Jobs.query.get(_id), jobs))
+
+        results = pd.DataFrame(columns=['Title', 'City', 'Date', 'Company Name',
+                                'Salary', 'Company Website', 'Source', 
+                                'Job Type', 'Location', 'Job Link'])
+        for job in myjobs:
+            results = results.append(
+                {
+                    'Title': job.job_title,
+                    'City' : job.city,
+                    'Date' : job.date_posted,
+                    'Company Name' : job.company_name,
+                    'Salary' : job.salary,
+                    'Company Website' : job.company_website,
+                    'Source' : job.source,
+                    'Job Type' : job.job_type,
+                    'Location' : job.job_location,
+                    'Job Link' : job.job_url,
+                }, ignore_index=True)
+
+        results.to_csv(mypath, index=False)
+        # return f"Success"
+        return send_from_directory(current_app.static_folder, filename='myjobs.csv', as_attachment=True)
+    if request.method == 'GET':
+        query = request.args.get('query').split()
+        location = request.args.get('location')
+        results = []
+        for q in query:
+            search = f"%{q}%"
+            que = Jobs.query.filter(Jobs.job_title.like(search)).filter \
+                            (Jobs.job_location.like(f"%{location}%")).all()
+            for i in que:
+                results.append(i)
+        keywords = ' '.join(query)
+        return render_template("main/results.html", jobs=results,
+                                length=len(results), keywords=keywords)
+
+@main.route('/csv', methods=["POST"])
+@login_required
+def csv_jobs():
+    jobs = request.form.getlist('csv_jobs')
+    mypath = 'myjobs.csv'
+    if os.path.exists(mypath): os.remove(mypath)
+    myjobs = list(map(lambda _id: Jobs.query.get(_id), jobs))
+
+    results = pd.DataFrame(columns=['Title', 'City', 'Date', 'Company Name',
+                            'Salary', 'Company Website', 'Source', 
+                            'Job Type', 'Location', 'Job Link'])
+    for job in myjobs:
+        results = results.append(
+            {
+                'Title': job.job_title,
+                'City' : job.city,
+                'Date' : job.date_posted,
+                'Company Name' : job.company_name,
+                'Salary' : job.salary,
+                'Company Website' : job.company_website,
+                'Source' : job.source,
+                'Job Type' : job.job_type,
+                'Location' : job.job_location,
+                'Job Link' : job.job_url,
+            }, ignore_index=True)
+
+    results.to_csv(mypath, index=False)
+    return send_from_directory('', filename=mypath, as_attachment=True)
+
+
+    
+    
--- myapp/scraper/routes.py
+++ myapp/scraper/routes.py
@@ -1,28 +1,19 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, request
+from numpy import source
 from myapp import db
-import pandas as pd
-from datetime import datetime
-from myapp.models import Jobs, ipAddress
-from subprocess import Popen
-import os
+from myapp.models import Jobs
+
 
 scraper = Blueprint('scraper', __name__)
 
-@scraper.route('/test')
-def test():
-    return render_template('test.html')
-# @scraper.route('/scrape_ips')
-# def scrape_ip():
-#     db.session.query(ipAddress).delete()
-#     db.session.commit()
-#     if os.path.exists('ip_addresses.csv'):
-#         os.remove('ip_addresses.csv')
-#     path = os.getcwd() + '\\myapp\\scraper\\utils\\ip_scraper.py'
-#     pro = Popen(['scrapy', 'runspider', path, '-o', 'ip_addresses.csv'])
-#     pro.wait()
-#     data = pd.read_csv('ip_addresses.csv')['ip']
-#     for i in data:
-#         ipad = ipAddress(ip= 'http://' + i)
-#         db.session.add(ipad)
-#     db.session.commit()
-#     return f"{data}"
+@scraper.route('/company')
+def company():
+    source = set([i.source for i in Jobs.query.all()])
+    return render_template('scraper/companies.html', source=source)
+
+@scraper.route('/company_jobs')
+def company_jobs():
+    source = request.args.get('source')
+    results = Jobs.query.filter_by(source=source).all()
+    return render_template("main/results.html", jobs=results,
+                            length=len(results), keywords=source)
